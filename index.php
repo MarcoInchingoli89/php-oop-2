94cd3286fe15f74f69d912120c772fb396930888
Marco Inchingoli <evilmarkus@hotmail.it>
2022-12-12 17:06:22 +0100

    Add instance of product

--- index.php
+++ index.php
@@ -10,36 +10,39 @@
 //Il cliente potrà sia comprare i prodotti come ospite, senza doversi registrarsi nello store, oppure può iscriversi e creare un account per ricevere cosi il 20% di sconto.
 //Il cliente effettua il pagamento dei prodotti nel carrello con la carta di credito, che non deve essere scaduta. */
 
-class Products
+class Product
 {
-    public function __construct(public string $title, public string $image, public int $price, public Categories $categories, public Types $types)
+    public function __construct(public string $title, public string $image, public int $price)
     {
         $this->title = $title;
         $this->image = $image;
         $this->price = $price;
-        $this->categories = $categories;
-        $this->types = $types;
     }
 }
 
-class Categories extends Products
-{
-    public $dogs;
-    public $cats;
-}
+$stick_di_pollo = new Product('Stick', 'Immagine.jpg', 5);
+var_dump($stick_di_pollo);
 
+/* class Food extends Product
+{
+public $calories;
+public $is_vegan;
+} */
+
+/* class Categories extends Products
+{
+public $dogs;
+public $cats;
+}
 class Types extends Categories
 {
-    public $foods;
-    public $houses;
-    public $harness;
-    public $toys;
-    public $accessories;
-    public $clothes;
-
-}
-
-$chicken_sticks = new Products('Stick di pollo', 'pollo.jpg', 5, )
+public $foods;
+public $houses;
+public $harness;
+public $toys;
+public $accessories;
+public $clothes;
+} */
 
 
 
@@ -49,4 +52,4 @@ $chicken_sticks = new Products('Stick di pollo', 'pollo.jpg', 5, )
 
 
 
-    ?>
+?>
