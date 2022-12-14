4daf877a0f71a1f17e93b8ecec149e3d7c080037
Marco Inchingoli <evilmarkus@hotmail.it>
2022-12-14 17:10:00 +0100

    Try to Add Exception (Not Work)

--- Models/Product.php
+++ Models/Product.php
@@ -12,14 +12,27 @@ class Product
         $this->category = $category;
         $this->type = $type;
     }
+
+/*  public function set_price(int $price)
+{
+try {
+if (($price) == 0) {
+throw new Exception("Il prezzo deve essere superiore a zero!");
+}
+$this->price = $price;
+} catch (Exception $e) {
+echo $e->getMessage();
+die();
+}
+} */
 }
 
-$stick_di_pollo = new Product('Stick di Pollo', 'Immagine.jpg', 5, new Category('Cane'), new Type('Cibo'));
+/* $stick_di_pollo = new Product('Stick di Pollo', 'Immagine.jpg', 5, new Category('Cane'), new Type('Cibo'));
 $scatoletta = new Product('Scatoletta', 'Immagine.jpg', 5, new Category('Gatto'), new Type('Cibo'));
 $topolino_giocattolo = new Product('Topolino Giocattolo', 'Immagine.jpg', 5, new Category('Gatto'), new Type('Giocattolo'));
 $cuccia = new Product('Cuccia', 'Immagine.jpg', 5, new Category('Cane'), new Type('Cuccia'));
 $guinzaglio = new Product('Guinzaglio', 'Immagine.jpg', 5, new Category('Cane'), new Type('Accessorio'));
-$pettorina = new Product('Pettorina', 'Immagine.jpg', 5, new Category('Cane'), new Type('Pettorina'));
+$pettorina = new Product('Pettorina', 'Immagine.jpg', 5, new Category('Cane'), new Type('Pettorina')); */
 /* var_dump($stick_di_pollo); */
 
 
